f830ca9f516087ed50a392cd864e14fcb292ddbd
Victor <victor@yourmajesty.co>
2024-06-20 00:37:53 +0300

    CNT-001, refactoring

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,5 @@
 "use client"
-import { useTheme } from "next-themes";
-
-import { themeColor } from "@/shared/helpers";
-
 
 export default function Home() {
-  const { theme } = useTheme();
-  return (<div style={themeColor(theme)}>vic</div>);
+  return (<div>vic</div>);
 }
